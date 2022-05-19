049d5caf26292be1280f2121a8ff552c6aadd5c5
dkozhuharova <dkozhuharova@SF-KOZHUHAROVA>
2022-05-19 13:25:00 +0300

    Add project files.

---
+++ Selenium_Web_Drivre_Demo/Selenium_Examples.cs
@@ -0,0 +1,16 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+var driver = new ChromeDriver();
+
+driver.Url = "https://wikipedia.org";
+
+Console.WriteLine("Current title:" + driver.Title);
+var searchField = driver.FindElement(By.Id("searchInput"));
+searchField.Click();
+searchField.SendKeys("QA");
+searchField.SendKeys(Keys.Enter);
+Console.WriteLine("New title:" + driver.Title);
+
+driver.Quit();
